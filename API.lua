15631f617217ca19647ab3a9e3c7bb268b37fcff
Error-Cezar <64325581+Error-Cezar@users.noreply.github.com>
2022-06-13 13:05:23 +0200

    Update API.lua

--- API.lua
+++ API.lua
@@ -328,7 +328,7 @@ function AddPart(part)
     a.ZIndex = 10
     a.Size = part.Size
     a.Transparency = 0.5
-    a.Color = part.Color
+    a.Color = part.BrickColor
     table.insert(ESPPart, part)
 end
 
